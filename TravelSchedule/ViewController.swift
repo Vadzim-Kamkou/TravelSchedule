bd607a396569b6d4cc2b91659d6ef37503752f33
Vadzim-Kamkou <vadzim.kamkou@gmail.com>
2025-10-31 18:57:31 +0300

    Initial Commit

---
+++ TravelSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelSchedule
+//
+//  Created by Vadzim on 31.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
